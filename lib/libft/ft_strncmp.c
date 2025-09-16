adfc7b59e3f221bb6a18b043a3e20e13d5f00002
SaraKteifan <skteifan@gmail.com>
2025-09-17 00:26:50 +0300

    Initial commit

---
+++ lib/libft/ft_strncmp.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skteifan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/02 08:44:46 by skteifan          #+#    #+#             */
+/*   Updated: 2025/04/22 16:09:18 by skteifan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s1 || !s2 || n <= 0)
+		return (0);
+	while (i < n)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			return (0);
+		if (s1[i] != s2[i])
+			return (((unsigned char *)s1)[i] -
+					((unsigned char *)s2)[i]);
+		i++;
+	}
+	return (0);
+}
